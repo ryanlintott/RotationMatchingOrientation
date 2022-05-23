d00d904a85f4667e0ac706191d00484b1594c12d
Ryan Lintott <ryan.lintott@abetterwaytodo.com>
2022-05-23 10:13:56 -0400

    Initial Commit

---
+++ RotationMatchingOrientation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RotationMatchingOrientation
+//
+//  Created by Ryan Lintott on 2022-05-23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RotationMatchingOrientation/RotationMatchingOrientationApp.swift
@@ -0,0 +1,17 @@
+//
+//  RotationMatchingOrientationApp.swift
+//  RotationMatchingOrientation
+//
+//  Created by Ryan Lintott on 2022-05-23.
+//
+
+import SwiftUI
+
+@main
+struct RotationMatchingOrientationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
